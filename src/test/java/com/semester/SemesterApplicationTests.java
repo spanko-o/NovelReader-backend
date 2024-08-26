7812be5b33a52d1b9814030b1d522e72ada89069
杨少凯 <3102425102@QQ.com>
2024-08-26 16:06:28 +0800

    初始框架

---
+++ src/test/java/com/semester/SemesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
