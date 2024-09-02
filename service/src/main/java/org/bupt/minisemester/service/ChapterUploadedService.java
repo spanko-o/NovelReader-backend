9955d35b73e62dc524c5e7dea40e7e778841b33a
cyber114514 <124226254+cyber114514@users.noreply.github.com>
2024-09-02 16:27:58 +0800

    完善详情页和阅读页的接口

--- service/src/main/java/org/bupt/minisemester/service/ChapterUploadedService.java
+++ service/src/main/java/org/bupt/minisemester/service/ChapterUploadedService.java
@@ -2,17 +2,44 @@ package org.bupt.minisemester.service;
 
 import org.bupt.minisemester.dao.entity.ChapterUploaded;
 import org.bupt.minisemester.dao.mapper.ChapterUploadedMapper;
-import org.bupt.minisemester.service.ChapterUploadedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface ChapterUploadedService {
-    ChapterUploaded getChapterById(int id);
-    List<ChapterUploaded> getAllChapters();
-    void saveChapter(ChapterUploaded chapter);
-    void updateChapter(ChapterUploaded chapter);
-    void deleteChapter(int id);
-}
+@Service
+public class ChapterUploadedService {
 
+    @Autowired
+    private ChapterUploadedMapper chapterUploadedMapper;
+
+    @Override
+    public ChapterUploaded getChapterById(int id) {
+        return chapterUploadedMapper.selectById(id);
+    }
+
+    @Override
+    public List<ChapterUploaded> getAllChapters() {
+        return chapterUploadedMapper.selectList(null);
+    }
+
+    @Override
+    public void saveChapter(ChapterUploaded chapter) {
+        chapterUploadedMapper.insert(chapter);
+    }
+
+    @Override
+    public void updateChapter(ChapterUploaded chapter) {
+        chapterUploadedMapper.updateById(chapter);
+    }
+
+    @Override
+    public void deleteChapter(int cid) {
+        chapterUploadedMapper.deleteById(cid);
+    }
+
+    @Override
+    public List<ChapterUploaded> getChapterByNid(Integer nid){
+        return chapterUploadedMapper.selectChapter(nid);
+    }
+}
