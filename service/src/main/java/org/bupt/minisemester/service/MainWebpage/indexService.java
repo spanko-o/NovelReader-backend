f51a8c54d1eb028f86bbe6cba13f2695541bc21b
wangwei <wangwei@e-byte.com>
2024-09-05 23:53:55 +0800

    [fix] page

--- service/src/main/java/org/bupt/minisemester/service/MainWebpage/indexService.java
+++ service/src/main/java/org/bupt/minisemester/service/MainWebpage/indexService.java
@@ -25,35 +25,43 @@ public class indexService {
     @Autowired
     private NovelMapper novelMapper;
 
+    /**
+     * 创建索引
+     */
     public void createIndex() {
         try {
-            // 删除旧的索引目录及文件
+            // 打开索引目录
             Directory directory = FSDirectory.open(Paths.get(IDX_DIR));
-            IndexWriterConfig config = new IndexWriterConfig(new SmartChineseAnalyzer());
+            // 创建分词器
+            SmartChineseAnalyzer analyzer = new SmartChineseAnalyzer();
+            // 配置IndexWriter
+            IndexWriterConfig config = new IndexWriterConfig(analyzer);
             IndexWriter indexWriter = new IndexWriter(directory, config);
 
-            // 删除所有现有索引
+            // 清空现有索引
             indexWriter.deleteAll();
 
-            // 从数据库获取数据并创建索引
+            // 从数据库获取数据
             List<Map<String, String>> novelList = novelMapper.selectNovel();
             for (Map<String, String> novel : novelList) {
+                // 创建文档
                 Document doc = new Document();
-                doc.add(new TextField("id", String.valueOf(novel.getOrDefault("id","")), Field.Store.YES));
-                doc.add(new TextField("title", novel.getOrDefault("title",""), Field.Store.YES));
-                doc.add(new TextField("description", novel.getOrDefault("description",""), Field.Store.YES));
-                doc.add(new TextField("author", novel.getOrDefault("author",""), Field.Store.YES));
-                doc.add(new TextField("noveltype", novel.getOrDefault("noveltype",""), Field.Store.YES));
-                doc.add(new TextField("picture", novel.getOrDefault("picture",""), Field.Store.YES));
+                doc.add(new TextField("id", String.valueOf(novel.getOrDefault("id", "")), Field.Store.YES));
+                doc.add(new TextField("title", novel.getOrDefault("title", ""), Field.Store.YES));
+                doc.add(new TextField("description", novel.getOrDefault("description", ""), Field.Store.YES));
+                doc.add(new TextField("author", novel.getOrDefault("author", ""), Field.Store.YES));
+                doc.add(new TextField("noveltype", novel.getOrDefault("noveltype", ""), Field.Store.YES));
+                doc.add(new TextField("picture", novel.getOrDefault("picture", ""), Field.Store.YES));
+
+                // 将文档添加到索引
                 indexWriter.addDocument(doc);
             }
 
+            // 提交更改并关闭索引写入器
             indexWriter.close();
-            System.out.println("索引重新创建完成！");
+            System.out.println("索引创建完成！");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
-
 }
--- service/src/main/java/org/bupt/minisemester/service/MainWebpage/searchService.java
+++ service/src/main/java/org/bupt/minisemester/service/MainWebpage/searchService.java
@@ -4,15 +4,14 @@ import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.FuzzyQuery;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.WildcardQuery;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.index.Term;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -29,13 +28,12 @@ public class searchService {
     private final static String IDX_DIR = "service/src/main/resources/index";
 
     /**
-     * 使用Lucene进行索引搜索
+     * 使用Lucene进行索引搜索，支持多个字段搜索
      * @param queryStr 查询的字符串
      * @return 返回搜索结果的列表，每个结果为一个Map对象
      * @throws IOException 如果读取索引文件时发生IO错误
-     * @throws ParseException 如果查询解析失败
      */
-    public List<Map<String, Object>> searchIndex(String queryStr) throws IOException, ParseException {
+    public List<Map<String, Object>> searchIndex(String queryStr) throws IOException {
         // 存储搜索结果的列表
         List<Map<String, Object>> resultList = new ArrayList<>();
 
@@ -46,42 +44,33 @@ public class searchService {
         // 创建IndexSearcher对象以执行搜索
         IndexSearcher indexSearcher = new IndexSearcher(indexReader);
 
-        // 创建Term对象以表示要搜索的字段和内容
-        Term term = new Term("title", queryStr);
-        // 使用FuzzyQuery进行模糊搜索，这允许搜索包含输入内容的相似词语
-        FuzzyQuery fuzzyQuery = new FuzzyQuery(term);
+        // 使用WildcardQuery进行宽松匹配，允许部分匹配
+        // 将输入的查询词加上通配符*进行模糊匹配，例如 "月*" 匹配以"月"开头的所有词
+        Query query = new WildcardQuery(new Term("title", queryStr + "*"));
 
-        // 选择要使用的查询对象，这里选择了模糊查询
-        Query query = fuzzyQuery;
-
-        // 执行搜索，返回TopDocs对象，包含匹配的文档
-        TopDocs docs = indexSearcher.search(query, 10); // 搜索前10条结果
+        // 执行搜索，获取所有匹配的文档
+        TopDocs docs = indexSearcher.search(query, Integer.MAX_VALUE); // 获取所有匹配的结果
 
         // 遍历搜索结果，将每个结果转换为Map并添加到resultList中
         for (ScoreDoc scoreDoc : docs.scoreDocs) {
-            if (scoreDoc.doc ==0) {
-                continue;
-            }
-            // 通过docId获取Document对象
             Document doc = indexSearcher.doc(scoreDoc.doc);
+
             // 创建Map对象存储该文档的相关信息
             Map<String, Object> result = new HashMap<>();
             result.put("id", doc.get("id")); // 存储文档的ID
-            result.put("title", doc.get("title")); // 存储文档的标题
-            result.put("description", doc.get("description")); // 存储文档的描述
-            result.put("author", doc.get("author")); // 存储文档的作者
-            result.put("noveltype", doc.get("noveltype")); // 存储文档的小说类型
-            result.put("picture", doc.get("picture")); // 存储文档的图片路径
-            result.put("score", scoreDoc.score); // 存储搜索得分
+            result.put("title", doc.get("title")); // 标题
+            result.put("description", doc.get("description")); // 文档的描述
+            result.put("author", doc.get("author")); // 文档的作者
+            result.put("noveltype", doc.get("noveltype")); // 文档的小说类型
+            result.put("picture", doc.get("picture")); // 文档的图片路径
+            result.put("score", scoreDoc.score); // 搜索得分
 
             // 将结果添加到结果列表中
             resultList.add(result);
         }
+
         // 关闭IndexReader以释放资源
         indexReader.close();
-
-        // 对结果进行排序，按得分降序排列
-        resultList.sort((r1, r2) -> Float.compare((Float) r2.get("score"), (Float) r1.get("score")));
 
         // 返回最终的搜索结果列表
         return resultList;
